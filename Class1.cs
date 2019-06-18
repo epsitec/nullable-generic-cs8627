0653e7e1d9d9dbd0af0eaa89abe1a92378be6fbc
Pierre Arnaud <arnaud@epsitec.ch>
2019-06-18 14:04:17 +0200

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace nullable_generic
+{
+    public class Class1
+    {
+//#nullable enable
+        public static bool Foo<TKey, TValue>(TKey key, TValue value, out TKey? output)
+               where TKey : class
+        {
+            throw new System.NotImplementedException ();
+        }
+    }
+}
